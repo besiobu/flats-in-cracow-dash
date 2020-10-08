9379ee1ae28c24cdad1f4940bb6c807d1eddc66c
besiobu <brian.esiobu@gmail.com>
2020-10-08 09:45:21 +0200

    Disable dropdown searchability and clearability.

--- app.py
+++ app.py
@@ -69,56 +69,72 @@ bathroom_slider = dcc.Slider(id='bathroom-slider', min=1, max=5,
 # Categorical dropdowns
 district_drop = dcc.Dropdown(id='district-dropdown', 
                              options=sorted(district_opts, key=lambda x: x['label']), 
-                             value='stare miasto')
+                             value='stare miasto', searchable=False, clearable=False)
 
 propterty_drop = dcc.Dropdown(id='property-dropdown', 
                               options=property_opts, 
-                              value='flat')
+                              value='flat', searchable=False, clearable=False)
 
 seller_drop = dcc.Dropdown(id='seller-dropdown', 
                            options=seller_opts, 
-                           value='realtor')
+                           value='realtor', searchable=False, clearable=False)
 
 parking_drop = dcc.Dropdown(id='parking-dropdown', 
                             options=sorted(parking_opts, key=lambda x: x['label']), 
-                            value='no parking')
+                            value='no parking', searchable=False, clearable=False)
 
 # Binary dropdowns
 garden_drop = dcc.Dropdown(id='garden-dropdown', 
                            options=bin_opts, 
-                           value=0)
+                           value=0,
+                           searchable=False, 
+                           clearable=False)
 
 basement_drop = dcc.Dropdown(id='basement-dropdown', 
                          options=bin_opts, 
-                         value=0)
+                         value=0,
+                         searchable=False, 
+                         clearable=False)
 
 balcony_drop = dcc.Dropdown(id='balcony-dropdown', 
                             options=bin_opts, 
-                            value=0)
+                            value=0,
+                            searchable=False, 
+                            clearable=False)
 
 terrace_drop = dcc.Dropdown(id='terrace-dropdown',
                             options=bin_opts, 
-                            value=0)
+                            value=0,
+                            searchable=False, 
+                            clearable=False)
 
 new_drop = dcc.Dropdown(id='new-dropdown',
                         options=bin_opts, 
-                        value=0)
+                        value=0,
+                        searchable=False, 
+                        clearable=False)
 
 block_drop = dcc.Dropdown(id='block-dropdown',
-                           options=bin_opts, 
-                           value=0)
+                          options=bin_opts, 
+                          value=0,
+                          searchable=False, 
+                          clearable=False)
 
 townhouse_drop = dcc.Dropdown(id='townhouse-dropdown',
                               options=bin_opts, 
-                              value=0)
+                              value=0,
+                              searchable=False, 
+                              clearable=False)
 
 apartment_drop = dcc.Dropdown(id='apartment-dropdown', 
                               options=bin_opts, 
                               value=0)
 
 busstop_drop = dcc.Dropdown(id='busstop-dropdown', 
-                         options=bin_opts, 
-                         value=0)
+                            options=bin_opts, 
+                            value=0,
+                            searchable=False, 
+                            clearable=False)
 
 studio_drop = dcc.Dropdown(id='studio-dropdown', 
                            options=bin_opts, 
